2f3f232000ffa1d1e680581dd23879effa848118
Christophe <christophecucchiara@sfr.fr>
2026-07-15 21:42:56 +0200

    Separate database helper and JSON data store

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,24 @@
 {
-  "users": [],
- "videos": []
+const fs = require('fs');
+const path = require('path');
+
+const dbPath = path.join(__dirname, 'db.json');
+
+function readDb() {
+  try {
+    const data = fs.readFileSync(dbPath, 'utf8');
+    return JSON.parse(data);
+  } catch (error) {
+    // Si le fichier n'existe pas ou est corrompu, on renvoie une structure vide
+    return { users: [], videos: [], comments: [] };
+  }
 }
+
+function writeDb(data) {
+  fs.writeFileSync(dbPath, JSON.stringify(data, null, 2), 'utf8');
+}
+
+module.exports = {
+  readDb,
+  writeDb
+};
